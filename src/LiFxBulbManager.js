06ec526790a42a58def1a1e04807d12b48027f1f
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-02-08 10:51:37 -0500

    move things to util, update state every 5 sec

--- src/LiFxBulbManager.js
+++ src/LiFxBulbManager.js
@@ -2,15 +2,71 @@ const lifx = require("node-lifx-lan");
 const _ = require("lodash");
 const colorConverter = require("color-convert");
 
+function generateQRCode(status) {
+  // left for debugging purposes
+  //  QRCode.toFile("./hack-bean-pot.png", JSON.stringify(status));
+
+  // TODO: convert to bitmap then ???? => display on screen
+  return QRCode.create(JSON.stringify(status));
+}
+
+// Returns an array of Promises, where each promise resolves to a JSON object.
+// Each object represents the state of a LIFX bulb in a LAN. The format of the
+// the array of JSON objects is:
+// [
+//  {
+//     color: { hue: 0.61181, saturation: 1, brightness: 0.01999, kelvin: 3500 },
+//     power: 1,
+//     label: 'Vibe Check ',
+//     infrared: null,
+//     multizone: null,
+//     chain: null
+//   }
+// ]
+// for more information on what these fields mean, check out:
+// https://www.npmjs.com/package/node-lifx-lan#getlightstate-method
+async function getStates() {
+  return lifx
+    .discover()
+    .then(device_list => {
+      return Promise.all(
+        device_list.map(device => {
+          return device.getLightState();
+        })
+      );
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
 /**
  * Convert a LiFx bulb state object to our capstone format.
- * Returns the `capstone-winners` format.
+ * Returns the status object in the `capstone-winners` format.
+ * {
+ *     "isOn": true,
+ *     "brightness": 0.01999,
+ *     "color": {
+ *        "r": 0,
+ *        "g": 0,
+ *        "b": 0,
+ *        "a": 1
+ *     },
+ *     "super": {
+ *        "status": "ok",
+ *        "deviceId": "Vibe Check ",
+ *        "deviceType": "lightbulb",
+ *        "location": "Trap House",
+ *        "group": "Toms Room"
+ *     }
+ *  }
  */
 function lifxStateToCapstone_Yeet(lifxState, lifxDeviceInfo) {
-  console.log(JSON.stringify(lifxDeviceInfo));
-  const rgb = colorConverter.hsl.rgb(lifxState.color.hue,
+  const rgb = colorConverter.hsl.rgb(
+    lifxState.color.hue,
     lifxState.color.saturation,
-    lifxState.color.brightness);
+    lifxState.color.brightness
+  );
   return {
     isOn: lifxState.power === 1,
     brightness: lifxState.color.brightness,
@@ -20,9 +76,9 @@ function lifxStateToCapstone_Yeet(lifxState, lifxDeviceInfo) {
       deviceId: lifxState.label,
       deviceType: "lightbulb",
       location: lifxDeviceInfo.location.label,
-      group: lifxDeviceInfo.group.label,
+      group: lifxDeviceInfo.group.label
     }
-  }
+  };
 }
 
 class LiFxBulbManager {
@@ -34,9 +90,7 @@ class LiFxBulbManager {
       this.bulb = await lifx
         .discover()
         .then(devices => {
-          const b = devices.find(
-            d => d["deviceInfo"]["label"] === deviceName
-          );
+          const b = devices.find(d => d["deviceInfo"]["label"] === deviceName);
           if (_.isNil(b)) {
             throw new Error(`Could not find bulb named ${deviceName}`);
           }
@@ -45,10 +99,41 @@ class LiFxBulbManager {
         .catch(err => {
           throw new Error("Could not discover devices " + err.message);
         });
+
       const lifxState = await this.bulb.getLightState();
-      this.bulbState = lifxStateToCapstone_Yeet(lifxState, this.bulb.deviceInfo);
+      this.bulbState = lifxStateToCapstone_Yeet(
+        lifxState,
+        this.bulb.deviceInfo
+      );
+
+      // TODO: remove console.log once we convert to bitmap and display on
+      // screen
+      console.log(generateQRCode(this.bulbState));
       return this;
     })(deviceName);
+  }
+
+  /**
+   * Update the internal state by querying LiFx LAN API. If the API returns
+   * a different state than what we currently have, update the QR code and
+   * display it on the screen.
+   */
+  async updateState() {
+    const stateResponses = await Promise.all([
+      this.bulb.getLightState(),
+      this.bulb.getDeviceInfo()
+    ]);
+    const newState = lifxStateToCapstone_Yeet(
+      stateResponses[0],
+      stateResponses[1]
+    );
+
+    if (!_.isEqual(this.bulbState, newState)) {
+      console.log("state has changed");
+      // the light bulb state has changed
+      this.bulbState = newState;
+      console.log(generateQRCode(this.bulbState));
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,62 +5,21 @@ const config = require("../config/config.js");
 const _ = require("lodash");
 const lifx = require("node-lifx-lan");
 const LiFxBulbManager = require("./LiFxBulbManager");
-
-
-function generateQRCode(status) {
-  // TODO: when using binary data must use a Uint8ClampedArray because:
-  // https://www.npmjs.com/package/qrcode#binary-data
-  // const dataArray = new Uint8ClampedArray([0, 1, 1, 1, 1, 1]);
-
-  // left for debugging purposes
-  QRCode.toFile("./hack-bean-pot.png", JSON.stringify(status));
-
-  // TODO: first argument of create can either be a string or a list of objects
-  // describing segments of the QR code.... not sure how to do the latter,
-  // so just stringify-ing a JSON right now
-  return QRCode.create(JSON.stringify(status));
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// Returns an array of Promises, where each promise resolves to a JSON object.
-// Each object represents the state of a LIFX bulb in a LAN. The format of the
-// the array of JSON objects is:
-// [
-//  {
-//     color: { hue: 0.61181, saturation: 1, brightness: 0.01999, kelvin: 3500 },
-//     power: 1,
-//     label: 'Vibe Check ',
-//     infrared: null,
-//     multizone: null,
-//     chain: null
-//   }
-// ]
-// for more information on what these fields mean, check out:
-// https://www.npmjs.com/package/node-lifx-lan#getlightstate-method
-async function getStates() {
-  return lifx
-    .discover()
-    .then(device_list => {
-      return Promise.all(device_list.map(device => {
-        return device.getLightState();
-      }))
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
-
+const util = require("./util");
 
 async function main() {
   console.log("hello world");
-//  const states = await getStates();
-//  console.log(states);
-//  generateQRCode(states[0]);
+  //  const states = await getStates();
+  //  console.log(states);
+  //  generateQRCode(states[0]);
   const bulbManager = await new LiFxBulbManager("Vibe Check ");
   console.log(JSON.stringify(bulbManager));
+
+  while (true) {
+    await util.sleep(5000);
+    bulbManager.updateState();
+    console.log("updated state");
+  }
 }
 
 main();
