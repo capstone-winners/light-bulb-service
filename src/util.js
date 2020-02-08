110a2c723ccf68a6714575acb1ae8cf57ddde678
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-02-08 14:57:18 -0500

    Fix hsv to rgb conversion

--- src/util.js
+++ src/util.js
@@ -1,7 +1,36 @@
-const colorConverter = require("color-convert");
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR 
+ * h, s, v
+*/
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
 }
 
 /**
@@ -26,7 +55,7 @@ function sleep(ms) {
  *  }
  */
 function lifxStateToCapstone_Yeet(lifxState, lifxDeviceInfo) {
-  const rgb = colorConverter.hsl.rgb(
+  const rgb = HSVtoRGB(
     lifxState.color.hue,
     lifxState.color.saturation,
     lifxState.color.brightness
