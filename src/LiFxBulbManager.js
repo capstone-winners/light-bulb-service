5301d6e9813201e8428aad14ef14840a6672fc1e
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-03-02 19:58:36 -0800

    Integrate with py-resize script to use e-ink display

--- src/LiFxBulbManager.js
+++ src/LiFxBulbManager.js
@@ -2,6 +2,7 @@ const lifx = require("node-lifx-lan");
 const _ = require("lodash");
 const QRCode = require("qrcode");
 const awsIot = require("aws-iot-device-sdk");
+const { spawn } = require('child_process');
 
 const config = require("../config/config.js");
 
@@ -57,9 +58,6 @@ class LiFxBulbManager {
       const bindedFunc = this.handleAction.bind(this);
       this.device.on("message", bindedFunc);
 
-      // TODO: remove console.log once we can correctly pad the image. Instead
-      // of logging this object, we will display it on the e-ink display.
-      console.debug(generateQRCode(this.bulbState));
       return this;
     })(deviceName);
   }
@@ -84,11 +82,11 @@ class LiFxBulbManager {
 
     if (!_.isEqual(this.bulbState, newState)) {
       // the light bulb state has changed
-      console.debug("state has changed");
+      console.debug("State has changed");
       this.bulbState = newState;
       // TODO: remove console.log once we can correctly pad the image. Instead
       // of logging this object, we will display it on the e-ink display.
-      console.debug(generateQRCode(this.bulbState));
+      generateQRCode(this.bulbState);
     }
   }
 
@@ -161,23 +159,18 @@ function generateErrorQRCode(message) {
 }
 
 /**
- * Generate a QR Code with the given status
+ * Generate a QR Code with the given status. Saves the generated QR code to
+ * a file and then calls the Python script with the file name which manipulates
+ * the image and then displays it on the e-ink display.
  */
-function generateQRCode(status) {
-  // left for debugging purposes
-  /*
-  QRCode.toFile('./bitmap.bmp', JSON.stringify(status), {
+function generateQRCode(status, deviceName) {
+  QRCode.toFile(`./${deviceName}.bmp`, JSON.stringify(status), {
     "width": 176
   }, function (err) {
-    if (err) throw err
-    console.log('done')
+    if (err) throw err;
+    console.debug('Saved a qr code from node');
   })
-  */
-
-  // TODO: call python script with QRCode data
-  // add zero'ed out buffer around image in Python
-  // then display image on e-ink display
-  return QRCode.create(JSON.stringify(status));
+  const pyProg = spawn('python', ['../py-resize.py', '-f', `./${deviceName}.bmp`]);
 }
 
 /**
